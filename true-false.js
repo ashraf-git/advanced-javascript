7ba9df659484c3ec179623129b7ba5712b922fa8
ashraf-git <aliashraf.hena@gmail.com>
2020-03-04 05:34:51 +0600

    true vs false.js

---
+++ true-false.js
@@ -0,0 +1,24 @@
+// const age = 0;
+// if(age){
+//     console.log("Condition is true.");
+// }
+// else{
+//     console.log("Condition is false.");
+// }
+// const name = "0";
+let name = 0;
+console.log(name);
+if(name || name == 0){
+    console.log("Condition is true.");
+}
+else{
+    console.log("Condition is false.");
+}
+
+// falsy
+// 0
+// null
+// undefined
+// NaN
+// ""
+// false
