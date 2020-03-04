4bcd28bb89cca948968ba8e741510fba74a97c65
ashraf-git <aliashraf.hena@gmail.com>
2020-03-04 14:51:27 +0600

    null-vs-undefined cleared for interview

---
+++ null-vs-undefined.js
@@ -0,0 +1,15 @@
+// let pakhi;
+// function add(sum1, sum2){
+//     console.log(sum1 + sum2);
+//     return
+// }
+// function add(sum1, sum2 = 19){
+//     console.log(sum1, sum2);
+// }
+
+// const result = {name: "ashraf", phone:8787};
+// console.log(result.gf);
+
+
+let fun = undefined;
+console.log(fun);
