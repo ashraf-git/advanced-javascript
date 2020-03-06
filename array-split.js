b26c55fb718601bdcf6b4cf7cbc4dc57ef367476
ashraf-git <aliashraf.hena@gmail.com>
2020-03-06 08:15:20 +0600

    Array slice, splice, array join elements

---
+++ array-split.js
@@ -0,0 +1,7 @@
+const nums = [1, 2, 3, 4, 5, 6, 7, 8];
+// const part = nums.slice(2, 6);
+// console.log(part);
+const removed = nums.splice(2, 5, 9, 10);
+// console.log(nums);
+const together = nums.join(" ");
+console.log(together);
