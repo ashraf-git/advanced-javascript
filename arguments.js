5a201a7d5ac8cc112a051a215802add5f0a4d26b
ashraf-git <aliashraf.hena@gmail.com>
2020-03-06 07:50:07 +0600

    Closure, encapsulation, private variable

--- arguments.js
+++ arguments.js
@@ -1,6 +1,7 @@
 function add(num1, num2){
     console.log([...arguments]);
     return num1 + num2 ;
+    
 }
 const result = add(2, 4, 6);
 
--- null-vs-undefined.js
+++ null-vs-undefined.js
@@ -1,14 +1,14 @@
-// let pakhi;
-// function add(sum1, sum2){
-//     console.log(sum1 + sum2);
-//     return
-// }
-// function add(sum1, sum2 = 19){
-//     console.log(sum1, sum2);
-// }
+let pakhi;
+function add(sum1, sum2){
+    console.log(sum1 + sum2);
+    return
+}
+function add(sum1, sum2 = 19){
+    console.log(sum1, sum2);
+}
 
-// const result = {name: "ashraf", phone:8787};
-// console.log(result.gf);
+const result = {name: "ashraf", phone:8787};
+console.log(result.gf);
 
 
 let fun = undefined;
